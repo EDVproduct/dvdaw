c066fd365eac8539881c1ee805721c14f406dd7f
davidvalles79 <davidvalles79@gmail.com>
2017-09-04 13:07:58 +0200

    he canviat de nou ,espero que puja aquest arxiu edv

---
+++ edv.php
@@ -0,0 +1,1 @@
+<?php " Hallo goeiemorgen dit archief is van Esther de Veer   en geschreven in het Nederlands enz.enz."; ?>
--- esther.php
+++ esther.php
@@ -1,1 +1,2 @@
 		<?php echo "Esther ha fet aquesta fitxer ";  ?>
+<?php echo " Aqui segeixo ,i ara he fet un altre en Holandès"; ?>
